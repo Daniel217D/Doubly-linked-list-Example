f45e3f9b902a268224b5da544bb49fd1e0c8fb55
Daniil <daniel217032001@gmail.com>
2020-10-07 00:43:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,42 @@
 #include <iostream>
+#include <fstream>
 
 #include "libraries/DList.h"
+#include "libraries/SickInfo.h"
 
 using std::cout;
+using std::ifstream;
 
 int main() {
-    auto list = new DList;
+    auto list = new DList<SickInfo>;
 
-    list->add_to_head(1);
-    list->add_to_head(2);
-    list->add_to_head(3);
-    list->print();
+    ifstream fin("files/input.txt");
 
-    list->add_to_tail(4);
-    list->add_to_tail(5);
-    list->add_to_tail(6);
-    list->print();
+    while(!fin.eof()) {
+        SickInfo temp;
+        fin >> temp.full_name.surname;
+        fin >> temp.full_name.name;
+        fin >> temp.full_name.father_name;
+        fin >> temp.job;
+        fin >> temp.number;
+        fin >> temp.date.day;
+        fin >> temp.date.month;
+        fin >> temp.date.year;
+        fin >> temp.incapacity_days;
 
-    Node *n = list->get_node()->get_next()->get_next();
-    list->remove(n);
+
+        if(list->is_empty() || !list->get_node()->get_info().date.compare(temp.date)) {
+            list->add_to_head(temp);
+        } else {
+            auto current = list->get_node();
+            while (current->get_next() != nullptr && current->get_next()->get_info().date.compare(temp.date)) {
+                current = current->get_next();
+            }
+            list->add_after(current, temp);
+        }
+
+    }
+
     list->print();
 
     delete list;
