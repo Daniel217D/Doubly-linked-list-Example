93dee498d5903225ec428818f39980c63f40a3f2
Daniil <daniel217032001@gmail.com>
2020-10-07 14:31:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,26 @@
 #include <iostream>
-#include <fstream>
-
-#include "libraries/DList.h"
 #include "libraries/SickInfo.h"
 
 using std::cout;
 using std::ifstream;
 
 int main() {
-    auto list = new DList<SickInfo>;
+    auto list = read_file_sick_list("files/input.txt");
+    if(!list->is_empty()) {
+        Date d;
+        cout << "Введите день, месяц и год: ";
+        d.console_set();
+        clean_sick_list(list, d);
 
-    ifstream fin("files/input.txt");
-
-    while(!fin.eof()) {
-        SickInfo temp;
-        fin >> temp.full_name.surname;
-        fin >> temp.full_name.name;
-        fin >> temp.full_name.father_name;
-        fin >> temp.job;
-        fin >> temp.number;
-        fin >> temp.date.day;
-        fin >> temp.date.month;
-        fin >> temp.date.year;
-        fin >> temp.incapacity_days;
-
-
-        if(list->is_empty() || !list->get_node()->get_info().date.compare(temp.date)) {
-            list->add_to_head(temp);
+        if(print_file_sick_list("files/output.txt", list)) {
+           cout << "Программа успешно завершена";
         } else {
-            auto current = list->get_node();
-            while (current->get_next() != nullptr && current->get_next()->get_info().date.compare(temp.date)) {
-                current = current->get_next();
-            }
-            list->add_after(current, temp);
+            cout << "Не удалось записать в файл";
         }
 
+    } else {
+        cout << "Не удалось считать из файла";
     }
-
-    list->print();
 
     delete list;
     return 0;
